4335c427680ff3ab33175eca7490507c11124bca
GoSnowball <julian.cayuela@etu.univ-amu.fr>
2021-04-22 15:23:24 +0200

    Cas général de HelloWorld

--- src/main/java/fr/univ_amu/iut/exercice1/HelloWorld.java
+++ src/main/java/fr/univ_amu/iut/exercice1/HelloWorld.java
@@ -4,8 +4,6 @@ public class HelloWorld {
     public static String hello(String s) {
         if (s == null || s.equals(""))
             return "Hello, World!";
-        if (s.equals("Alice"))
-            return "Hello, Alice!";
-        return "";
+        return "Hello, " + s + "!";
     }
 }
