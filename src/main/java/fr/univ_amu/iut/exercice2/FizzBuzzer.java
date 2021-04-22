054bad25173c4a3aa904b91b236a98903644fdc5
GoSnowball <julian.cayuela@etu.univ-amu.fr>
2021-04-22 16:16:28 +0200

    Retourne une chaine si non multiple de 3 ou 5 et retourne Fizz si multiple de 3 et Buzz si multiple de 5 et si multiple de 15 retourne FizzBuzz

--- src/main/java/fr/univ_amu/iut/exercice2/FizzBuzzer.java
+++ src/main/java/fr/univ_amu/iut/exercice2/FizzBuzzer.java
@@ -3,7 +3,7 @@ package fr.univ_amu.iut.exercice2;
 public class FizzBuzzer {
 
     public String computeString(int i) {
-        if (i == 15)
+        if (i % 15 == 0)
             return "FizzBuzz";
         if (i % 3 == 0)
             return "Fizz";
